481bb815008e989c3bb092771c399848601dd7d4
Lyrecoph <lyrecoph30@gmail.com>
2024-08-12 18:58:29 +0100

    creating and providing the context

---
+++ src/store/shopping-cart-context.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+// cette variable contient un composant react dont ns aurons plus tard
+// on peut aussi passer une valeur à createContext qui sera utilisé comme 
+// une valeur initiale qui peut être fournie à plusieurs composants 
+export const CartContext = createContext({
+    // stock les articles de mon panier d'achat
+    items: []
+})
